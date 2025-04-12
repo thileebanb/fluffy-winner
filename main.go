7165b653c9b2cc3013dc6913809f9edf80332e43
Thileeban Balasundaram <thileeban.b@marketsimplified.com>
2025-04-12 14:28:41 +0530

    feat: add basic web api

--- main.go
+++ main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-
-	"rsc.io/quote"
+	"log"
+	"net/http"
 )
 
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+var users = []User{
+	{ID: 1, Name: "Alice"},
+	{ID: 2, Name: "Bob"},
+}
+
+// Handler to return all users
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
+}
+
+// Handler for homepage
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to the Go API!")
+}
+
 func main() {
-	fmt.Println(quote.Opt())
+	http.HandleFunc("/", home)
+	http.HandleFunc("/users", getUsers)
+
+	port := ":8080"
+	fmt.Println("Server running on http://localhost" + port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
